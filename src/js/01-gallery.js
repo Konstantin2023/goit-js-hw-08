969050d97ee99f8eb4390705ac591c44bb4b2c85
Konstantin2023 <pscosx@gmail.com>
2023-01-04 17:56:44 +0200

    Перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,45 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(SimpleLightbox);
+
+// import { galleryItems } from "./gallery-items.js";
+// // Change code below this line
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const cards = cardsGalleryItems(galleryItems);
+gallery.insertAdjacentHTML('beforeend', cards);
+
+function cardsGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join('');
+}
+gallery.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const urlLargeImage = evt.target.getAttribute('data-source');
+  console.log(urlLargeImage);
+  const instance = basicLightbox.create(
+    `<img src="${urlLargeImage}" width="800" height="600">`
+  );
+  instance.show();
+});
