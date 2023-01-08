d3ce0f1ea0254b5ed27e74a322a78f50b80f6abc
Konstantin2023 <pscosx@gmail.com>
2023-01-08 16:59:03 +0200

    Домашня робота 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,22 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 console.log(Player);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const CURRENT_TIME = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+setCurrentTime();
+
+function setCurrentTime() {
+  if (localStorage.getItem(CURRENT_TIME)) {
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+  }
+}
